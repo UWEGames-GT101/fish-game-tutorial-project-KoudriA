8f7015d7f65b8c5462d0fda4974dadc0ead85dd8
Adam <adam2.koudri@live.uwe.ac.uk>
2023-10-05 12:53:35 +0100

    changed background colours, initialised background

--- tutorial_game.py
+++ tutorial_game.py
@@ -1,6 +1,7 @@
 import random
 import pyasge
 from gamedata import GameData
+help(pysage.COLOURS)
 
 
 def isInside(sprite, mouse_x, mouse_y) -> bool:
@@ -20,7 +21,7 @@ class MyASGEGame(pyasge.ASGEGame):
             settings (pyasge.GameSettings): The game settings
         """
         pyasge.ASGEGame.__init__(self, settings)
-        self.renderer.setClearColour(pyasge.COLOURS.CORNFLOWER)
+        self.renderer.setClearColour(pyasge.COLOURS.GOLDERNROD)
 
         # create a game data object, we can store all shared game content here
         self.data = GameData()
@@ -55,7 +56,11 @@ class MyASGEGame(pyasge.ASGEGame):
         self.initFish()
 
     def initBackground(self) -> bool:
-        pass
+        if self.data.background.loadTexture("/data/images/background.png"):
+            self.data.background.z_order = -100
+            return True
+        else:
+            return False
 
     def initFish(self) -> bool:
         pass
